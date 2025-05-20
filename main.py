35d153c78d21fea44e24200be0860f349f86c2f9
crown-town-clown <63020400+crown-town-clown@users.noreply.github.com>
2025-05-20 11:07:02 -0700

    added word count

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def get_book_text(file_path):
 		file_contents = f.read()
 	return file_contents
 
+def num_words():
+	words = get_book_text("books/frankenstein.txt").split()
+	return len(words) 
+
 def main():
-	print(get_book_text("books/frankenstein.txt"))
+	print(f"{num_words()} words found in the document")
 
 main()
